6aaa008aa57dbe20424561c4ec91831436193218
Jacob A. Shumard <jacobshu62@ou.edu>
2019-05-01 14:23:56 -0500

    created all components

--- GraphicalHammingDistance.java
+++ GraphicalHammingDistance.java
@@ -1,5 +1,6 @@
 import java.awt.GridLayout;
 import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 import java.io.File;
@@ -16,16 +17,44 @@ import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
 import javax.swing.JTextField;
 import javax.swing.filechooser.FileSystemView;
 
 
 
 
-public class GraphicalHammingDistance 
+public class GraphicalHammingDistance extends JFrame
 {
-
+	
+	
+	private JLabel hammingDistanceInfo;
+	private JLabel distance0;
+	private JLabel distance1;
+	private JLabel distance2;
+	private JLabel distance3;
+	private JLabel distance4;
+	private JLabel compareWith;
+	
+	private JTextField addStationText;
+	
+	
+	private JButton showStation;
+	private JButton calculateHD;
+	private JButton addStation;
+	
+	private JSlider slider;
+	
+	private JComboBox box;
+	
+	private Rectangle sliderbox;
+	private Rectangle distancebox0;
+	private Rectangle distancebox1;
+	private Rectangle distancebox2;
+	private Rectangle distancebox3;
+	private Rectangle distancebox4;
 	
 	
 	
 }
+
